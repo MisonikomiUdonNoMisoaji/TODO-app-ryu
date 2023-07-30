6f5864f81ca6784683295016851180e439660908
Ryu-nakay <ryukenkana@gmail.com>
2023-07-30 14:25:44 +0900

    first commit

---
+++ TODOApp/TODOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOAppApp.swift
+//  TODOApp
+//
+//  Created by cw-ryu.nakayama on 2023/07/30.
+//
+
+import SwiftUI
+
+@main
+struct TODOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
